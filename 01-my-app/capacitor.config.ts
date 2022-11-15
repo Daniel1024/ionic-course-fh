f5277d38e52c33b40d10bd68fb15f3edd747b796
Daniel Lopez <d.lopez.1740@gmail.com>
2022-11-15 19:56:03 -0300

    replace name

--- 01-my-app/capacitor.config.ts
+++ 01-my-app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '01-my-app',
+  appName: 'my-app',
   webDir: 'www',
   bundledWebRuntime: false
 };
