c0baa63ad6c40653be4a260bc5bd26cd8f45ad2b
Daniel Lopez <d.lopez.1740@gmail.com>
2022-11-27 13:28:02 -0300

    add compnents

--- 03-componentes/src/app/pages/inicio/inicio.page.ts
+++ 03-componentes/src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IComponent {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  components: IComponent[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'action sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'alert',
+      redirectTo: '/alert',
+    },
+  ];
 
   constructor() { }
 
